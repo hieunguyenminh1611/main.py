2bf54001a689842d88dc02f1d33b6314fad53ea3
hieunguyenminh1611 <60960739+hieunguyenminh1611@users.noreply.github.com>
2020-02-12 15:06:10 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+#Song Information and Facts, declared as Variables
+Genre ="R & B"
+Artist = "Mary J. Blige"
+Song = "My Life"
+AlbumTitle = "My Life"
+DurationInSeconds = 257
+YearOfRelease = 1994
+MusicRecordCompany = "Geffen Records"
+ExecutiveProducers = "Sean (Diddy) Combs, Andre Harrell"
+
+#Print all song information and facts
+print(Genre)
+print(Artist)
+print(Song)
+print(AlbumTitle)
+print(DurationInSeconds)
+print(YearOfRelease)
+print(MusicRecordCompany)
+print(ExecutiveProducers)
